766e77322fb96c2226651edce1b7d9cd2b032cf8
stheophil <sebastian@theophil.net>
2013-08-28 23:26:27 +0200

    Import Spreadsheet, Prettify Code, add loader_io

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,157 +14,199 @@ import scala.collection.JavaConversions._
 import models._
 import views._
 
-object Application extends Controller with Secured {  
-  // Authentification
-  val loginForm = Form(
-    tuple(
-      "email" -> text,
-      "password" -> text
-    ) verifying ("Ungültige E-Mail oder falsches Passwort", result => result match {
-      case (email, password) => User.authenticate(email, password).isDefined
-    })
-  )
-  
-  def login = HTTPS { implicit request =>
-    Ok(html.login(loginForm))
-  }
+object Application extends Controller with Secured {
+	// Authentification
+	val loginForm = Form(
+		tuple(
+			"email" -> text,
+			"password" -> text) verifying ("Ungültige E-Mail oder falsches Passwort", result => result match {
+				case (email, password) => User.authenticate(email, password).isDefined
+			}))
 
-  def authenticate = Action { implicit request =>
-    loginForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(html.login(formWithErrors)),
-      user => Redirect(routes.Application.index).withSession("email" -> user._1)
-    )
-  }
+	def login = HTTPS { implicit request =>
+		Ok(html.login(loginForm))
+	}
 
-  def logout = Action {
-    Redirect(routes.Application.login).withNewSession.flashing(
-      "success" -> "Du wurdest erfolgreich ausgeloggt"
-    )
-  }
+	def authenticate = Action { implicit request =>
+		loginForm.bindFromRequest.fold(
+			formWithErrors => BadRequest(html.login(formWithErrors)),
+			user => Redirect(routes.Application.index).withSession("email" -> user._1))
+	}
 
-  // Index page
-  private def CountPerRating(l: List[Statement]) : Map[Int, Int] = {
-    l.groupBy(_.rating.id).map(p => (p._1, p._2.length))
-  }
-  
-  def index = Action { implicit request =>
-    val stmts = Statement.allWithoutEntries()
-    val catresultmap = stmts.groupBy(_.category.name).map( p => (p._1, CountPerRating(p._2) ) ) + ("" -> CountPerRating(stmts))
-    Ok( views.html.index(stmts, catresultmap, username(request) flatMap { User.load(_) } ) )
-  }
-  
-  // Entry page / entry editing  
-  val newEntryForm = Form(
-    tuple(
-      "content" -> text.verifying ("Kein Text eingegeben", c => !c.isEmpty),
-      "stmt_id" -> number.verifying ( id => Statement.load(id).isDefined )
-    )
-  )
-  
-  def view(id: Long) = Action { implicit request => 
-    // TODO: Use newEntryForm to generate form
-    val optstmt = Statement.load(id)
-    optstmt match {
-      case Some(stmt) => Ok(views.html.detail(stmt, newEntryForm, username(request) flatMap { User.load(_) }))
-      case None => NotFound
-    }    
-  }
-  
-  def addEntry = IsEditor { user => implicit request =>
-    newEntryForm.bindFromRequest.fold(
-        formWithErrors => {
-          formWithErrors.error("stmt_id") match {
-            case Some(e) => Redirect(routes.Application.index).flashing("error" -> "Ungültige Anfrage")
-            case None => {
-              val stmt_id = formWithErrors("stmt_id").value.get
-              val stmt = Statement.load(Integer.parseInt(stmt_id)).get // both Options must be valid if stmt_id verified ok
-              Ok(views.html.detail(stmt, formWithErrors, Some(user)))
-            }
-          }           
-        },
-        t => {
-          Entry.create(t._2, t._1, new java.util.Date(), user.id)
-          Redirect(routes.Application.view(t._2))
-        }
-    )
-  }
+	def logout = Action {
+		Redirect(routes.Application.login).withNewSession.flashing(
+			"success" -> "Du wurdest erfolgreich ausgeloggt")
+	}
 
-  def loadSpreadSheet(spreadsheet: String) = IsAdmin { user => implicit request =>      
-    var categorymap = collection.mutable.Map.empty[String, anorm.Pk[Long]]
-    var stmtlist = new collection.mutable.ListBuffer[(String, String, String)]
-    var count = 0
+	// Index page
+	private def CountPerRating(l: List[Statement]): Map[Int, Int] = {
+		l.groupBy(_.rating.id).map(p => (p._1, p._2.length))
+	}
 
-    try {
-      val service = new SpreadsheetService("import");
+	def index = Action { implicit request =>
+		val stmts = Statement.allWithoutEntries()
+		val catresultmap = stmts.groupBy(_.category.name).map(p => (p._1, CountPerRating(p._2))) + ("" -> CountPerRating(stmts))
+		Ok(views.html.index(stmts, catresultmap, username(request) flatMap { User.load(_) }))
+	}
 
-      // Define the URL to request.  This should never change.
-      val WORKSHEET_FEED_URL = new java.net.URL(
-          "http://spreadsheets.google.com/feeds/worksheets/" + spreadsheet + "/public/values");
+	// Entry page / entry editing  
+	val newEntryForm = Form(
+		tuple(
+			"content" -> text.verifying("Kein Text eingegeben", c => !c.isEmpty),
+			"stmt_id" -> number.verifying(id => Statement.load(id).isDefined)))
 
-      val worksheet = service.getFeed(WORKSHEET_FEED_URL, classOf[WorksheetFeed]).getEntries().get(0);
-      val listFeed = service.getFeed(worksheet.getListFeedUrl(), classOf[ListFeed]);
-      
-      // Iterate through each row, printing its cell values.
-     
-      for (row <- listFeed.getEntries()) {
-        val custom = row.getCustomElements()
-        
-        val category = categorymap.getOrElseUpdate(
-          custom.getValue("ressort"),
-          anorm.NotAssigned
-        )
-        stmtlist += ( (custom.getValue("titel"), custom.getValue("ressort"), custom.getValue("ergebnis")) )
-        count=count+1
-      }
+	def view(id: Long) = Action { implicit request =>
+		val optstmt = Statement.load(id)
+		optstmt match {
+			case Some(stmt) => Ok(views.html.detail(stmt, newEntryForm, username(request) flatMap { User.load(_) }))
+			case None => NotFound
+		}
+	}
 
-      Ok(stmtlist.toString)
-    } catch {
-      case e: Exception  => Ok(e.toString)
-    }     
-  }
+	def addEntry = IsEditor { user => implicit request =>
+		newEntryForm.bindFromRequest.fold(
+			formWithErrors => {
+				formWithErrors.error("stmt_id") match {
+					case Some(e) => Redirect(routes.Application.index).flashing("error" -> "Ungültige Anfrage")
+					case None => {
+						val stmt_id = formWithErrors("stmt_id").value.get
+						val stmt = Statement.load(Integer.parseInt(stmt_id)).get // both Options must be valid if stmt_id verified ok
+						Ok(views.html.detail(stmt, formWithErrors, Some(user)))
+					}
+				}
+			},
+			t => {
+				Entry.create(t._2, t._1, new java.util.Date(), user.id)
+				Redirect(routes.Application.view(t._2))
+			})
+	}
+
+	class ImportException(message: String) extends java.lang.Exception(message)
+	
+	def loadSpreadSheet(spreadsheet: String) = IsAdmin { user => implicit request =>
+		try {
+			val service = new SpreadsheetService("import");
+
+			// Define the URL to request.  This should never change.
+			val WORKSHEET_FEED_URL = new java.net.URL(
+				"http://spreadsheets.google.com/feeds/worksheets/"+spreadsheet+"/public/values");
+
+			val worksheet = service.getFeed(WORKSHEET_FEED_URL, classOf[WorksheetFeed]).getEntries().get(0);
+			val listFeed = service.getFeed(worksheet.getListFeedUrl(), classOf[ListFeed]);
+
+			// Iterate through each row, printing its cell values.
+			var mapExistingCategories = collection.mutable.Map.empty[String, Category]
+			mapExistingCategories ++= (for(c <- Category.loadAll()) yield (c.name, c))
+			
+			var mapNewCategories = collection.mutable.Map.empty[String, Long] // new category name -> order
+			var nCategoryOrder = mapExistingCategories.values.maxBy(_.order).order
+
+			var setTags = collection.mutable.Set.empty[String]
+
+			var nImported = 0
+			var cStatements = collection.mutable.ArrayBuffer.empty[(String, String, Option[String], Option[String])] // Titel, Ressort, Zitat, Quelle
+
+			for (row <- listFeed.getEntries()) {
+				val custom = row.getCustomElements()
+
+				val strCategory = custom.getValue("ressort")
+				if (strCategory == null) throw new ImportException("Fehlendes Ressort bei Wahlversprechen Nr. "+(nImported+1))
+
+				if (mapExistingCategories.contains(strCategory)) {
+					mapNewCategories.getOrElseUpdate(
+						strCategory,
+						{
+							nCategoryOrder = nCategoryOrder + 1
+							nCategoryOrder
+						}
+					)
+				}
+
+				val strTitle = custom.getValue("titel")
+				if (strTitle == null) throw new ImportException("Fehlender Titel bei Wahlversprechen Nr. "+(nImported+1))
+
+				val strQuote = if (custom.getValue("zitat") == null) None else Some(custom.getValue("zitat"))
+				val strSource = if (custom.getValue("quelle") == null) None else Some(custom.getValue("quelle"))
+				cStatements += ( (strTitle, strCategory, strQuote, strSource) )
+
+				if (custom.getValue("tags") != null) custom.getValue("tags").split(',').foreach(setTags += _)
+
+				nImported = nImported + 1
+			}
+
+			import play.api.Play.current
+			play.api.db.DB.withTransaction { c =>
+				mapNewCategories.foreach(t => {
+					val category = Category.create(c, t._1, t._2)
+					mapExistingCategories += (t._1 -> category)
+				})
+
+				// TODO insert tags, 
+				// TODO quote, source
+				cStatements.foreach(t => {
+					Statement.create(c, t._1, mapExistingCategories.get(t._2).get, Rating.Unrated)
+				})
+			}
+
+			Redirect(routes.Application.index).flashing("success" -> (nImported+" Wahlversprechen erfolgreich importiert."))
+		} catch {
+			case e: ImportException => {
+				Redirect(routes.Application.index).flashing("error" -> e.getMessage())
+			}
+			case e: Exception => {
+				Logger.error(e.toString)
+				Logger.error(e.getStackTraceString)
+				Redirect(routes.Application.index).flashing("error" -> "Beim Importieren ist ein Fehler aufgetreten.")
+			}
+		}
+	}
+	
+	def loader_io = Action {
+		Ok("loaderio-1d023df4783a421bb574cfc64791a5c4")
+	}
 }
-
 
 /**
  * Provide security features
  */
-trait Secured {  
-  def username(request: RequestHeader) = request.session.get("email")
-  
-  def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Application.login)
-  
-  def IsAuthenticated(f: => String => Request[AnyContent] => Result) = Security.Authenticated(username, onUnauthorized) { user =>
-    Action(request => f(user)(request))
-  }
+trait Secured {
+	def username(request: RequestHeader) = request.session.get("email")
 
-  def IsAdmin(f: => User => Request[AnyContent] => Result) = IsAuthenticated { username => request =>
-    val u = User.load(username)
-    if(u.isDefined && u.get.role == Role.Admin) {
-      f(u.get)(request)
-    } else {
-      Results.Forbidden
-    }
-  }
+	def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Application.login)
 
-  def IsEditor(f: => User => Request[AnyContent] => Result) = IsAuthenticated { username => request =>
-    val u = User.load(username)
-    if(u.isDefined && (u.get.role == Role.Admin || u.get.role==Role.Editor)) { 
-      f(u.get)(request)
-    } else {
-      Results.Forbidden
-    }
-  }
+	def IsAuthenticated(f: => String => Request[AnyContent] => Result) = Security.Authenticated(username, onUnauthorized) { user =>
+		Action(request => f(user)(request))
+	}
 
-  /** Called before every request to ensure that HTTPS is used. */
-  def HTTPS(f: => Request[AnyContent] => Result) = Action { request =>
-    import play.api.Play.current
-    if (Play.isDev 
-    || request.headers.get("x-forwarded-proto").isDefined
-    && request.headers.get("x-forwarded-proto").get.contains("https"))
-    {
-      f(request)
-    } else {
-      Results.Redirect("https://" + request.host + request.uri);
-    }
-  }  
+	def IsAdmin(f: => User => Request[AnyContent] => Result) = IsAuthenticated { username =>
+		request =>
+			val u = User.load(username)
+			if (u.isDefined && u.get.role == Role.Admin) {
+				f(u.get)(request)
+			} else {
+				Results.Forbidden
+			}
+	}
+
+	def IsEditor(f: => User => Request[AnyContent] => Result) = IsAuthenticated { username =>
+		request =>
+			val u = User.load(username)
+			if (u.isDefined && (u.get.role == Role.Admin || u.get.role == Role.Editor)) {
+				f(u.get)(request)
+			} else {
+				Results.Forbidden
+			}
+	}
+
+	/** Called before every request to ensure that HTTPS is used. */
+	def HTTPS(f: => Request[AnyContent] => Result) = Action { request =>
+		import play.api.Play.current
+		if (Play.isDev
+			|| request.headers.get("x-forwarded-proto").isDefined
+			&& request.headers.get("x-forwarded-proto").get.contains("https")) {
+			f(request)
+		} else {
+			Results.Redirect("https://"+request.host + request.uri);
+		}
+	}
 }
--- app/models/Statement.scala
+++ app/models/Statement.scala
@@ -1,13 +1,13 @@
 package models
 
 object Rating extends Enumeration {
-  type Rating = Value
-  val PromiseKept, Compromise, PromiseBroken, Stalled, InTheWorks, Unrated = Value
+	type Rating = Value
+	val PromiseKept, Compromise, PromiseBroken, Stalled, InTheWorks, Unrated = Value
 };
 
 object Role extends Enumeration {
-  type Role = Value
-  val Admin, Editor, Unprivileged = Value
+	type Role = Value
+	val Admin, Editor, Unprivileged = Value
 };
 
 import Role._
@@ -27,170 +27,178 @@ case class User(id: Long, email: String, name: String, password: String, salt: S
 case class Entry(id: Long, stmt_id: Long, content: String, date: Date, user: User)
 case class Statement(id: Long, title: String, category: Category, entries: List[Entry], rating: Rating)
 
+object Category {
+	val category = {
+		get[Long]("id") ~
+			get[String]("name") ~
+			get[Long]("order") map {
+				case id ~ name ~ order => Category(id, name, order)
+			}
+	}
 
-object Category {  
-	def create(name: String, order: Long) : Category = {
+	def create(name: String, order: Long): Category = {
+		DB.withConnection { implicit c => create(c, name, order) }
+	}
+
+	def create(implicit connection: java.sql.Connection, name: String, order: Long): Category = {
+		val id: Long = SQL("select nextval('cat_id_seq')").as(scalar[Long].single)
+
+		SQL("insert into category values ({id}, {name}, {order})").on(
+			'id -> id,
+			'name -> name,
+			'order -> order).executeUpdate()
+
+		Category(id, name, order)
+	}
+
+	def loadAll(): List[Category] = {
 		DB.withConnection { implicit c =>
-      val id: Long = SQL("select nextval('cat_id_seq')").as(scalar[Long].single)
-	  
-        SQL("insert into category values ({id}, {name}, {order})").on(
-          'id -> id,
-	        'name -> name,
-	        'order -> order
-	      ).executeUpdate()
-
-        Category(id, name, order)
-	    }
-	} 
+			SQL("select * from category").as(category*)
+		}
+	}
 }
 
-object User {  
-  val user = {
-    get[Long]("id") ~
-	  get[String]("email") ~ 
-	  get[String]("name") ~
-	  get[String]("password") ~
-    get[String]("salt") ~
-	  get[Int]("role") map {
-	    case id~email~name~password~salt~role => User(id, email, name, password, salt, if(0<=role && role<Role.maxId) Role(role) else Unprivileged)
-	  }
-  }
-  
-  def load(id: Long) : Option[User] = {
-    DB.withConnection { implicit c =>
-      SQL("select * from users where id = {id}").on('id -> id).as(user.singleOpt)
-    }
-  }
+object User {
+	val user = {
+		get[Long]("id") ~
+			get[String]("email") ~
+			get[String]("name") ~
+			get[String]("password") ~
+			get[String]("salt") ~
+			get[Int]("role") map {
+				case id ~ email ~ name ~ password ~ salt ~ role => User(id, email, name, password, salt, if (0 <= role && role < Role.maxId) Role(role) else Unprivileged)
+			}
+	}
 
-  def load(email: String) : Option[User] = {
-    DB.withConnection { implicit c =>
-      SQL("select * from users where email = {email}").on('email -> email).as(user.singleOpt)
-    }
-  }
-  
-  def findAll() : List[User] = {
-    DB.withConnection { implicit c =>
-      SQL("select * from users").as(user*)
-    }
-  }
+	def load(id: Long): Option[User] = {
+		DB.withConnection { implicit c =>
+			SQL("select * from users where id = {id}").on('id -> id).as(user.singleOpt)
+		}
+	}
 
-  private def passwordhash(salt: String, password: String) : String = {
-     val md = java.security.MessageDigest.getInstance("SHA-1")
-     new sun.misc.BASE64Encoder().encode( md.digest( (salt + password).getBytes) )
-  }
-  
-  def create(email: String, name: String, password: String, role: Role) : User = {
-    DB.withConnection { implicit c =>
-      val id: Long = SQL("select nextval('user_id_seq')").as(scalar[Long].single)
-      val salt = (for(i <- 1 to 20) yield util.Random.nextPrintableChar).mkString
-      val hash = passwordhash(salt, password)
-      SQL("insert into users values ({id}, {email}, {name}, {password}, {salt}, {role})").on(
-        'id -> id,
-        'email -> email,
-        'name -> name,
-        'password -> hash,
-        'salt -> salt,
-        'role -> role.id
-      ).executeUpdate()
+	def load(email: String): Option[User] = {
+		DB.withConnection { implicit c =>
+			SQL("select * from users where email = {email}").on('email -> email).as(user.singleOpt)
+		}
+	}
 
-      User(id, email, name, hash, salt, role)
-    }
-  }
-  
-  def authenticate(email: String, password: String): Option[User] = {
-    DB.withConnection { implicit connection =>
-      User.load(email) filter (u => u.password == passwordhash(u.salt, password))
-    }
-  }
-}  
+	def findAll(): List[User] = {
+		DB.withConnection { implicit c =>
+			SQL("select * from users").as(user*)
+		}
+	}
+
+	private def passwordhash(salt: String, password: String): String = {
+		val md = java.security.MessageDigest.getInstance("SHA-1")
+		new sun.misc.BASE64Encoder().encode(md.digest((salt + password).getBytes))
+	}
+
+	def create(email: String, name: String, password: String, role: Role): User = {
+		DB.withConnection { implicit c =>
+			val id: Long = SQL("select nextval('user_id_seq')").as(scalar[Long].single)
+			val salt = (for (i <- 1 to 20) yield util.Random.nextPrintableChar).mkString
+			val hash = passwordhash(salt, password)
+			SQL("insert into users values ({id}, {email}, {name}, {password}, {salt}, {role})").on(
+				'id -> id,
+				'email -> email,
+				'name -> name,
+				'password -> hash,
+				'salt -> salt,
+				'role -> role.id).executeUpdate()
+
+			User(id, email, name, hash, salt, role)
+		}
+	}
+
+	def authenticate(email: String, password: String): Option[User] = {
+		DB.withConnection { implicit connection =>
+			User.load(email) filter (u => u.password == passwordhash(u.salt, password))
+		}
+	}
+}
 
 object Entry {
-  val entry = {
-    get[Long]("id") ~
-    get[Long]("stmt_id") ~ 
-    get[String]("content") ~
-    get[Date]("date") ~
-    get[Long]("user_id") map {
-      case id~stmt_id~content~date~user_id => Entry(id, stmt_id, content, date, User.load(user_id).get)
-    }
-  }
-  
-  def loadByStatement(stmt_id: Long) : List[Entry] = {
-    DB.withConnection { implicit c =>
-      SQL("select * from entry where stmt_id = {stmt_id} ORDER by date DESC").on('stmt_id -> stmt_id).as(entry*)
-    }
-  }
-  
-  def create(stmt_id: Long, content: String, date: Date, user_id: Long) {
-    DB.withTransaction { implicit c =>
-       val id = SQL("select nextval('entry_id_seq')").as(scalar[Long].single)
-       
-       SQL(
-         """
+	val entry = {
+		get[Long]("id") ~
+			get[Long]("stmt_id") ~
+			get[String]("content") ~
+			get[Date]("date") ~
+			get[Long]("user_id") map {
+				case id ~ stmt_id ~ content ~ date ~ user_id => Entry(id, stmt_id, content, date, User.load(user_id).get)
+			}
+	}
+
+	def loadByStatement(stmt_id: Long): List[Entry] = {
+		DB.withConnection { implicit c =>
+			SQL("select * from entry where stmt_id = {stmt_id} ORDER by date DESC").on('stmt_id -> stmt_id).as(entry*)
+		}
+	}
+
+	def create(stmt_id: Long, content: String, date: Date, user_id: Long) {
+		DB.withTransaction { implicit c =>
+			val id = SQL("select nextval('entry_id_seq')").as(scalar[Long].single)
+
+			SQL(
+				"""
            insert into entry values (
              {id}, {stmt_id}, {content}, {date}, {user_id}
            )
-         """
-       ).on(
-         'id -> id,
-         'stmt_id -> stmt_id,
-         'content -> content,
-         'date -> date,
-         'user_id -> user_id
-       ).executeUpdate()
-     } 
-  }
+         """).on(
+					'id -> id,
+					'stmt_id -> stmt_id,
+					'content -> content,
+					'date -> date,
+					'user_id -> user_id).executeUpdate()
+		}
+	}
 }
 
-object Statement {  
-  val stmt = {
-	  get[Long]("statement.id") ~ 
-	  get[String]("statement.title")  ~
-	  get[Long]("category.id") ~
-	  get[String]("category.name") ~
-	  get[Long]("category.ordering") ~
-	  get[Int]("statement.rating") map {
-	    case id~title~category_id~category_name~category_order~rating => Statement(id, title, Category(category_id, category_name, category_order), List[Entry](), if(0<=rating && rating<Rating.maxId) Rating(rating) else Unrated)
-	  } // Rating(rating) would throw java.util.NoSuchElementException
-  }
-  
-  var query = """select statement.id, title, rating, category.id, category.name, category.ordering from statement join category on category.id=cat_id"""
+object Statement {
+	val stmt = {
+		get[Long]("statement.id") ~
+			get[String]("statement.title") ~
+			get[Long]("category.id") ~
+			get[String]("category.name") ~
+			get[Long]("category.ordering") ~
+			get[Int]("statement.rating") map {
+				case id ~ title ~ category_id ~ category_name ~ category_order ~ rating => Statement(id, title, Category(category_id, category_name, category_order), List[Entry](), if (0 <= rating && rating < Rating.maxId) Rating(rating) else Unrated)
+			} // Rating(rating) would throw java.util.NoSuchElementException
+	}
 
-  def allWithoutEntries(): List[Statement] = {
-    DB.withConnection( { implicit c =>
-      SQL(query + " order by category.ordering").as(stmt *)
-     } )
-  }
-  
-  def load(id: Long) : Option[Statement] = {
-    // TODO: Create stmt with entries in the first place
-    val s = DB.withConnection( { implicit c => 
-      SQL(query + " where statement.id = {id}").on(
-          'id -> id
-      ).as(stmt.singleOpt)
-    } )
-    s map { stmt => Statement(stmt.id, stmt.title, stmt.category, Entry.loadByStatement(id), stmt.rating) }    
-  } 
-  
-  def create(title: String, cat: Category, rating: Rating) : Statement = {
-    DB.withTransaction { implicit c =>
-       // Get the project id
-       val id: Long = SQL("select nextval('stmt_id_seq')").as(scalar[Long].single)       
-       // Insert the project
-       SQL(
-         """
-           insert into statement values (
-             {id}, {title}, {cat_id}, {rating}
-           )
-         """
-       ).on(
-         'id -> id,
-         'title -> title,
-         'cat_id -> cat.id,
-         'rating -> rating.id
-       ).executeUpdate()
-       
-       Statement(id, title, cat, List[Entry](), rating)
-     }
-  }
+	var query = """select statement.id, title, rating, 
+		category.id, category.name, category.ordering 
+		from statement 
+		join category on category.id=cat_id"""
+
+	def allWithoutEntries(): List[Statement] = {
+		DB.withConnection({ implicit c =>
+			SQL(query+" order by category.ordering").as(stmt *)
+		})
+	}
+
+	def load(id: Long): Option[Statement] = {
+		// TODO: Create stmt with entries in the first place
+		val s = DB.withConnection({ implicit c =>
+			SQL(query+" where statement.id = {id}").on(
+				'id -> id).as(stmt.singleOpt)
+		})
+		s map { stmt => Statement(stmt.id, stmt.title, stmt.category, Entry.loadByStatement(id), stmt.rating) }
+	}
+
+	def create(title: String, cat: Category, rating: Rating): Statement = {
+		DB.withTransaction { implicit c => Statement.create(c, title, cat, rating) }
+	}
+
+	def create(implicit connection: java.sql.Connection, title: String, cat: Category, rating: Rating): Statement = {
+		// Get the project id
+		val id: Long = SQL("select nextval('stmt_id_seq')").as(scalar[Long].single)
+		// Insert the project
+		SQL("insert into statement values ({id}, {title}, {cat_id}, {rating})").on(
+				'id -> id,
+				'title -> title,
+				'cat_id -> cat.id,
+				'rating -> rating.id).executeUpdate()
+
+		Statement(id, title, cat, List[Entry](), rating)
+	}
 }
